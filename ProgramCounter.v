6fff44f1ca82f793509214e3c81dac90b99e732d
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-22 19:35:13 -0800

    Program Counter forwards the input to the output on the posedge of the clk

---
+++ ProgramCounter.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:35:34 01/20/2019 
+// Design Name: 
+// Module Name:    ProgramCounter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ProgramCounter( input [31:0] PCin,
+							  input clk,
+							  output reg [31:0] PCout);
+							  
+	always @(posedge clk) begin
+		PCout <= PCin;
+	end
+	
+endmodule
