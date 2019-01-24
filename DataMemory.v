b2fbdd8a79be995e10a4252482db8db4b6d43150
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-23 20:08:59 -0800

    Initialized the data memory with initial values for TESTING PURPOSES.

--- DataMemory.v
+++ DataMemory.v
@@ -20,24 +20,24 @@
 //////////////////////////////////////////////////////////////////////////////////
 module DataMemory( input clk, 
 						 input WE,
-						 input A,
+						 input [31:0] A,
 						 input WD,
 						 output [31:0] RD );
 						 
 	reg [31:0] mem [63:0];
 	
 	initial begin 
-		mem[0] = 0;
-		mem[1] = 1;
-		mem[2] = 2;
-		mem[3] = 3;
-		mem[4] = 4; 
+		mem[0] = 10;
+		mem[1] = 9;
+		mem[2] = 8;
+		mem[3] = 7;
+		mem[4] = 6; 
 		mem[5] = 5;
-		mem[6] = 6;
-		mem[7] = 7;
-		mem[8] = 8;
-		mem[9] = 9;
-		mem[10] = 10;
+		mem[6] = 4;
+		mem[7] = 3;
+		mem[8] = 2;
+		mem[9] = 1;
+		mem[10] = 0;
 	end 
 	
 	assign RD = mem[A];
