667844f078380e9a629dcff0efc64bc9e4310d2d
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-23 21:00:05 -0800

    At the rising edge of the clock, check IF the write enable for memory is 1. IF so, write the data at input WD to memory address A.

--- DataMemory.v
+++ DataMemory.v
@@ -21,26 +21,32 @@
 module DataMemory( input clk, 
 						 input WE,
 						 input [31:0] A,
-						 input WD,
+						 input [31:0] WD,
 						 output [31:0] RD );
 						 
 	reg [31:0] mem [63:0];
 	
 	initial begin 
-		mem[0] = 10;
-		mem[1] = 9;
-		mem[2] = 8;
-		mem[3] = 7;
-		mem[4] = 6; 
-		mem[5] = 5;
-		mem[6] = 4;
-		mem[7] = 3;
-		mem[8] = 2;
-		mem[9] = 1;
+		mem[0] = 0;
+		mem[1] = 0;
+		mem[2] = 0;
+		mem[3] = 0;
+		mem[4] = 0; 
+		mem[5] = 0;
+		mem[6] = 0;
+		mem[7] = 0;
+		mem[8] = 0;
+		mem[9] = 0;
 		mem[10] = 0;
 	end 
 	
 	assign RD = mem[A];
+	
+	// Write data into memory address A when the Write Enable is ON.
+	always @(posedge clk) begin
+		if (WE)
+		mem[A] <= WD;
+	end 
 
 
 endmodule
