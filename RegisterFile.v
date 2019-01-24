c7398f97774458bd299f718dc6a58cb5ae8009af
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-23 20:07:03 -0800

    Initialized all 32 registers to 0.

--- RegisterFile.v
+++ RegisterFile.v
@@ -27,17 +27,54 @@ module RegisterFile( input clk,
 							output [31:0] RD1, 
 							output [31:0] RD2 );
 	
-	reg [31:0] file [31:0];
+	reg [31:0] registers [31:0];
 	
+	// Initialize file to 0s. 
+	initial begin 
+	registers[0] = 32'h00000000;
+	registers[1] = 32'h00000000;
+	registers[2] = 32'h00000000;
+	registers[3] = 32'h00000000;
+	registers[4] = 32'h00000000;
+	registers[5] = 32'h00000000;
+	registers[6] = 32'h00000000;
+	registers[7] = 32'h00000000;
+	registers[8] = 32'h00000000;
+	registers[9] = 32'h00000000;
+	registers[10] = 32'h00000000;
+	registers[11] = 32'h00000000;
+	registers[12] = 32'h00000000;
+	registers[13] = 32'h00000000;
+	registers[14] = 32'h00000000;
+	registers[15] = 32'h00000000;
+	registers[16] = 32'h00000000;
+	registers[17] = 32'h00000000;
+	registers[18] = 32'h00000000;
+	registers[19] = 32'h00000000;
+	registers[20] = 32'h00000000;
+	registers[21] = 32'h00000000;
+	registers[22] = 32'h00000000;
+	registers[23] = 32'h00000000;
+	registers[24] = 32'h00000000;
+	registers[25] = 32'h00000000;
+	registers[26] = 32'h00000000;
+	registers[27] = 32'h00000000;
+	registers[28] = 32'h00000000;
+	registers[29] = 32'h00000000;
+	registers[30] = 32'h00000000;
+	registers[31] = 32'h00000000;
+
+	
+	end 
 	// Writes into the register file happen if RegWrite enable = 1. 
 	// Updates after posedge of clk.
 	always @(posedge clk) begin
 		if (RegWrite) begin
-			file[A3] <= WD3;
+			registers[A3] <= WD3;
 		end
 	end
 	
 	// Assign the base address to RD1. 
-	assign RD1 = file[A1];
+	assign RD1 = registers[A1];
 	
 endmodule
