d44439ef95dd607bf2308dc9b346550df0890ba0
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-23 20:16:39 -0800

    Checkpoint: Supports load word instructions

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -21,7 +21,10 @@
 module ProgramCounter( input [31:0] PCin,
 							  input clk,
 							  output reg [31:0] PCout);
-							  
+	initial begin 
+		PCout <= 0;
+	end 
+	
 	always @(posedge clk) begin
 		PCout <= PCin;
 	end
--- TopLevel.v
+++ TopLevel.v
@@ -18,31 +18,32 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module TopLevel (input clk, // Clock Control
+module TopLevel (input master_clk, // Clock Control
 					  input [2:0] ALUControl, // ALU Control 
 					  input RegWrite // Control on Register Write 
+					  
 					  );
 					  
-	wire [31:0] PCout_Addr_Plus4; 
-	wire [31:0] Plus4_PCin;
+	wire [31:0] PCout_Addr_Plus1; 
+	wire [31:0] Plus1_PCin;
 	wire [31:0] Whole_RD; 
 	wire [31:0] RD1_SrcA;
 	wire [31:0] SignExtend_SrcB;
 	wire [31:0] ALUResult_DMAddress;
 	wire [31:0] DMReadData_RFWriteData3;
 	
-	ProgramCounter U00 ( .PCin(Plus4_PCin), .clk(clk), .PCout(PCout_Addr_Plus4) );
+	ProgramCounter U00 ( .PCin(Plus1_PCin), .clk(master_clk), .PCout(PCout_Addr_Plus1) );
 	
-	PCPlus4 U01 ( .PCin(PCout_Addr_Plus4), .PCout(Plus4_PCin) );
+	PCPlus1 U01 ( .PCin(PCout_Addr_Plus1), .PCout(Plus1_PCin) );
 	
-	InstructionMemory U02 ( .A(PCout_Addr_Plus4), .RD(Whole_RD) );
+	InstructionMemory U02 ( .A(PCout_Addr_Plus1), .RD(Whole_RD) );
 	
-	RegisterFile U03 ( .clk(clk), .RegWrite(RegWrite), .A1(Whole_RD[25:21]), .A2(A2), .A3(Whole_RD[20:16]), .WD3(DMReadData_RFWriteData3), .RD1(RD1_SrcA), .RD2(RD2) );
+	RegisterFile U03 ( .clk(master_clk), .RegWrite(RegWrite), .A1(Whole_RD[25:21]), .A2(A2), .A3(Whole_RD[20:16]), .WD3(DMReadData_RFWriteData3), .RD1(RD1_SrcA), .RD2(RD2) );
 	
 	SignExtend U04 ( .a(Whole_RD[15:0]), .signImm(SignExtend_SrcB) );
 	
 	ArithmeticLogicUnit U05 ( .SrcA(RD1_SrcA), .SrcB(SignExtend_SrcB), .ALUControl(ALUControl), .ALUResult(ALUResult_DMAddress), .ZeroFlag(ZeroFlag) );
 	
-	DataMemory U06 ( .clk(clk), .WE(WE), .A(ALUResult_DMAddress), .WD(WD), .RD(DMReadData_RFWriteData3) );
+	DataMemory U06 ( .clk(master_clk), .WE(WE), .A(ALUResult_DMAddress), .WD(WD), .RD(DMReadData_RFWriteData3) );
 
 endmodule
