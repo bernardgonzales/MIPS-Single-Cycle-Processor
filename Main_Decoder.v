ad3d4f925d4f5d4fd3cc6792253f76a7ad2bae16
Bernard Gonzales <bernard.luke.gonzales@gmail.com>
2019-01-26 15:54:17 -0800

    Added support for addi and jump instructions.

--- Main_Decoder.v
+++ Main_Decoder.v
@@ -25,29 +25,24 @@ module Main_Decoder( input [5:0] Opcode,
 							output ALUSrc, 
 							output RegDst, 
 							output RegWrite,
-							output [1:0] ALUOp
+							output [1:0] ALUOp,
+							output Jump
 							);
 							
-	reg [7:0] control_output; 
+	reg [8:0] control_output; 
 	//reg [2:0] alu_operation;
 	always @(*) begin 
 		case (Opcode) 
-		6'b100011: control_output <= 8'b10100100;  // Load Word
-		6'b101011: control_output <= 8'b00101000; // Store Word
-		6'b000100: control_output <= 8'b00010001; // Beq 
-		6'b000000: control_output <= 8'b11000010; // R-type instruction
-
+		6'b100011: control_output <= 9'b101001000;  // LW
+		6'b101011: control_output <= 9'b001010000; // SW
+		6'b000100: control_output <= 9'b000100010; // Beq 
+		6'b000000: control_output <= 9'b110000100; // R-type 
+		6'b001000: control_output <= 9'b101000000; // addi  
+		6'b000010: control_output <= 9'b000000001; // Jump 
 		endcase
 	end 
 	
 	// Assign Opcode to the respective multiplexers and write enables. 
-	assign { RegWrite, RegDst, ALUSrc, Branch, MemWrite, MemtoReg, ALUOp[1:0] } = control_output [7:0] ;
-	/*
-	assign RegWrite = control_output[7];
-	assign RegDst = control_output[6]; 
-	assign ALUSrc = control_output[5];
-	assign Branch = control_output[4];
-	assign MemWrite = control_output[3];
-	assign MemtoReg = control_output[2];
-	assign ALUOp[2:0] = control_output[1:0];*/
+	assign { RegWrite, RegDst, ALUSrc, Branch, MemWrite, MemtoReg, ALUOp[1:0], Jump } = control_output [8:0] ;
+
 endmodule
